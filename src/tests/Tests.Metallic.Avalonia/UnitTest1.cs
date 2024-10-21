4c030e2672162dc605bdf527e724564b1e0d8e0d
mik3c <mike.carleton@gmail.com>
2024-10-21 12:25:28 -0400

    Add new projects and tests to metallic.sln
    - Added `Metallic.Mvvm`, `Tests.Metallic.Mvvm`, `Metallic.Avalonia`, and `Tests.Metallic.Avalonia` to `metallic.sln`.
    - Updated solution configurations to include Debug and Release for new projects.
    - Nested new projects appropriately in the solution file.
    - Created project files for `Metallic.Mvvm` and `Metallic.Avalonia` using `Microsoft.NET.Sdk`.
    - Created test project files for `Tests.Metallic.Mvvm` and `Tests.Metallic.Avalonia` with necessary package references and settings.
    - Added initial unit test files `UnitTest1.cs` for both test projects.

---
+++ src/tests/Tests.Metallic.Avalonia/UnitTest1.cs
@@ -0,0 +1,8 @@
+﻿namespace Tests.Metallic.Avalonia {
+	public class UnitTest1 {
+		[Fact]
+		public void Test1() {
+
+		}
+	}
+}
---
+++ src/tests/Tests.Metallic.Mvvm/UnitTest1.cs
@@ -0,0 +1,8 @@
+﻿namespace Tests.Metallic.Mvvm {
+	public class UnitTest1 {
+		[Fact]
+		public void Test1() {
+
+		}
+	}
+}
